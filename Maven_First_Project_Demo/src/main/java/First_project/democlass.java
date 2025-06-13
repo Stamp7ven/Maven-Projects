93823a846c286be8982474c38cb8d213cc475463
Stamp7ven <151641830+Stamp7ven@users.noreply.github.com>
2025-06-13 03:29:01 +0000

    a lot

--- Maven_First_Project_Demo/src/main/java/First_project/democlass.java
+++ Maven_First_Project_Demo/src/main/java/First_project/democlass.java
@@ -6,4 +6,10 @@ public class democlass {
 		return "hello";
 	}
 
+	public static void main(String[] args) {
+        System.out.println("Running from democlass main method!");
+        democlass myDemo = new democlass(); // สร้าง Object ของ democlass
+        System.out.println(myDemo.demo());  // เรียกใช้เมธอด demo()
+    }
+
 }
